cc67dcfbea8b5c530dd60bb1acc7ca57b295e45a
Michael Montanez <mmontanez@umass.edu>
2025-06-23 17:45:56 -0400

    Set up folder structure and Tailwind

---
+++ template/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles/index.css'
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Portfolio Project</h1>
+    </div>
+  );
+}
+
+export default App
