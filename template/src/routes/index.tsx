48bc8860c3fa3e300b94531e078e5e2651c55b81
supriyaahejib <supriyaahejib@gmail.com>
2025-07-22 14:14:46 +0300

    Added interactive background

--- template/src/routes/index.tsx
+++ template/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Typewriter } from 'react-simple-typewriter'
 import AboutSection from '../routes/about'
 import ProjectsSection from '../routes/project'
 import ExperienceSection from '../routes/experience'
+import BubbleBackground from '../bubblebackground'
 
 export const Route = createFileRoute('/')({
   component: RouteComponentMainPage,
@@ -10,18 +11,27 @@ export const Route = createFileRoute('/')({
 
 function RouteComponentMainPage() {
   return (
-    <div>
+    <div className="relative overflow-hidden">
+      {/* Bubble Background */}
+      <BubbleBackground />
+
       {/* Landing Page Section */}
       <section
         id="home"
-        className="h-screen flex flex-col justify-center items-center text-center px-6 pt-0"
+        className="min-h-screen flex flex-col justify-center items-center text-center px-6 relative z-10"
       >
         <div className="-translate-y-12">
           <h1 className="text-5xl font-extrabold mb-6 tracking-tight">
-            Hi, I'm{" "}
+            Hi, I'm {' '}
             <span className="text-[#a3747e]">
               <Typewriter
-                words={['Your Name', 'a CodeCollab Dev', 'a Designer', 'a Developer', 'a Student']}
+                words={[
+                  'Your Name',
+                  'a CodeCollab Dev',
+                  'a Designer',
+                  'a Developer',
+                  'a Student',
+                ]}
                 loop={true}
                 cursor
                 cursorStyle="|"
@@ -32,17 +42,20 @@ function RouteComponentMainPage() {
             </span>
           </h1>
           <p className="text-lg text-[#2d2d2d] max-w-xl">
-            Welcome to my personal portfolio built with CodeCollab. Explore my work and connect with me.
+            Welcome to my personal portfolio built with CodeCollab. Explore my
+            work and connect with me.
           </p>
         </div>
 
-        {/* Arrow positioned absolutely at the bottom */}
-        <a
-          href="#about"
-          className="absolute bottom-10 animate-bounce text-3xl text-[#a3747e]"
-        >
-          ↓
-        </a>
+        {/* Arrow */}
+        <div className="mt-24">
+          <a
+            href="#about"
+            className="animate-bounce text-3xl text-[#a3747e]"
+          >
+            ↓
+          </a>
+        </div>
       </section>
 
       {/* About Section */}
@@ -54,5 +67,5 @@ function RouteComponentMainPage() {
       {/* Projects Section */}
       <ProjectsSection />
     </div>
-  );
+  )
 }
